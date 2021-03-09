d5fe9c5c41fd2ce74fae8d6d36ccce65d725735c
junia7759 <79143127+junia7759@users.noreply.github.com>
2021-03-09 20:38:09 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discodo
+from discord.ext import commands
+
+import cogs
+
+print(discodo.__version__)
+import logging
+
+logging.basicConfig(level=logging.INFO)
+# logging.getLogger("discodo").setLevel(logging.DEBUG)
+
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.Audio = discodo.DPYClient(self)
+
+        cogs.load(self)
+        self.load_extension("jishaku")
+
+
+if __name__ == "__main__":
+    bot = Bot(command_prefix="!")
+    bot.Audio.register_local_node()
+    access_token = os.environ["BOT_TOKEN"]
+    bot.run(access_token)
